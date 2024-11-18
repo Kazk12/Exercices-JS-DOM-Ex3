4e7d07dfa135d76f91225a741307b4284ab84465
Kazk12 <kazuko43KO@gmail.com>
2024-11-18 15:59:52 +0100

    MODIFY : Exo3

--- Exercice_3/main.js
+++ Exercice_3/main.js
@@ -1,30 +1,32 @@
-// const boutons = document.querySelectorAll(".color");
-// const text = document.querySelector("#text");
-
-// boutons.forEach(bouton => {
-//     bouton.addEventListener("click", handleClickChangeCouleur);
-// })
-
-// function handleClickChangeCouleur
-
-
-
+const boutons = document.querySelectorAll(".color");
 const text = document.querySelector("#text");
 
-const vert = document.querySelector("div .green");
-const rouge = document.querySelector("div .red");
-const bleu = document.querySelector("div .blue");
+boutons.forEach(bouton => {
+    bouton.addEventListener("click", handleClickChangeCouleur);
+})
 
-vert.addEventListener("click", handleClickTexteVert);
-rouge.addEventListener("click", handleClickTexteRouge);
-bleu.addEventListener("click", handleClickTexteBleu);
+function handleClickChangeCouleur(event){
+    text.style.color = event.target.classList[1];
+}
 
-function handleClickTexteVert(){
-    text.style.color = ("green");
-}
-function handleClickTexteRouge(){
-    text.style.color =("red");
-}
-function handleClickTexteBleu(){
-    text.style.color = ("blue");
-}
+
+
+// const text = document.querySelector("#text");
+
+// const vert = document.querySelector("div .green");
+// const rouge = document.querySelector("div .red");
+// const bleu = document.querySelector("div .blue");
+
+// vert.addEventListener("click", handleClickTexteVert);
+// rouge.addEventListener("click", handleClickTexteRouge);
+// bleu.addEventListener("click", handleClickTexteBleu);
+
+// function handleClickTexteVert(){
+//     text.style.color = ("green");
+// }
+// function handleClickTexteRouge(){
+//     text.style.color =("red");
+// }
+// function handleClickTexteBleu(){
+//     text.style.color = ("blue");
+// }
